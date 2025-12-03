98da45779396bb1ef959b646363ddc512c297fca
YoriMaster <yorismaster@outlook.com>
2025-12-03 23:33:35 +1100

    update

--- 4_2P/server.js
+++ 4_2P/server.js
@@ -31,31 +31,6 @@ const ProjectSchema = new mongoose.Schema({
 });
 const Project = mongoose.model('Project', ProjectSchema);
 
-
-// const cardList = [
-//     {
-//         title: "2010 Holden Cruze",
-//         image: "images/Holden.jpg",
-//         linkText: "2023.10 - 2024.8",
-//         link: "https://www.facebook.com/share/16Tr79hvGd/",
-//         desciption: "Water tank issue, need to replace the whole engine cooling system"
-//     },
-//     {
-//         title: "2008 Audi TT",
-//         image: "images/Audi.jpg",
-//         linkText: "2024.8 - 2025.6",
-//         link: "https://www.facebook.com/share/15S2Vp59EHE/",
-//         desciption: "Air intake issue, couldn't solve it"
-//     },
-//     {
-//         title: "2010 Golf TSI",
-//         image: "images/VW.jpg",
-//         linkText: "2025.6 - now",
-//         link: "#",
-//         desciption: "German car, best car"
-//     }
-// ]
-
 app.get("/api/projects", async (req, res) => {
     const projects = await Project.find({});
     res.json({ statusCode: 200, data: Project, message: "success" })
